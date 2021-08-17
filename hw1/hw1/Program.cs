db7aa19ca6150a96fc01dd2a6c27968d37d1e58c
sadgass <88186249+sadgass@users.noreply.github.com>
2021-08-17 20:32:43 +0500

    Add files via upload

---
+++ hw1/hw1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите логин");
+            string guestlogin = Console.ReadLine();
+            string login = "qwe321";
+
+            
+            if (guestlogin.Length < 2 || guestlogin.Length > 10)
+            {
+                Console.WriteLine("Логин не верного формата");
+            }
+          
+            bool correct = true;
+
+            if (Char.IsDigit(guestlogin[0]))
+            {
+                correct = false;
+                return;
+
+            }
+            
+
+            if (correct && guestlogin == login)
+            {
+                Console.WriteLine("Открыт доступ к системе");
+            }
+            else
+            {
+                Console.WriteLine("Логин не верный");
+            }
+
+        }
+    }
+}
---
+++ hwex3/hwex2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hwex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            replacetest();
+        }
+
+        private static void replacetest()
+        {
+            Console.WriteLine("Введите 1-ю последовательность");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Введите 2-ю последовательность");
+            string s2 = Console.ReadLine();
+
+            bool res = s1.Select(Char.ToUpper).OrderBy(x => x).SequenceEqual(s2.Select(Char.ToUpper).OrderBy(x => x));
+
+            if (res)
+            {
+                Console.WriteLine("Строки состоят из одинаковых букв");
+            }
+            else
+            {
+                Console.WriteLine("Строки не состоят из одинаковых букв");
+            }
+        }
+    }
+}
